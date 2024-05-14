7d6b92cbbbec31173928ba0f681a3eff4fb57cd1
dhernandez00242 <38764168+dhernandez00242@users.noreply.github.com>
2024-05-14 11:58:25 +0200

    Delete TaskClient directory

--- TaskClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
