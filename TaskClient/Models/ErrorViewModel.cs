52f85291ac8011f585b319acb745547e3a589709
dhernandez00242 <38764168+dhernandez00242@users.noreply.github.com>
2024-05-14 12:00:49 +0200

    Add TaskClient
    Add TaskClient

---
+++ TaskClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
